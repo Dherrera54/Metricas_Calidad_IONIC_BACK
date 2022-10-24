b6886902a4768493b8a7ed83bbb0a916cf8349ed
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-10-24 18:09:28 -0500

    Updated structure 3

--- flaskr/app.py
+++ flaskr/app.py
@@ -5,15 +5,7 @@ from .vistas import  VistaCancionesUsuario, VistaCancion, VistaSignIn, VistaAlbu
        VistaComentariosCancionesUsuario, VistaComentariosDeCancion, VistaNotificacion, VistaNotificacionesUsuario
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
-from flask import Flask
-
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'frase-secreta'
-    app.config['PROPAGATE_EXCEPTIONS'] = True
-    return app
+from flaskr import create_app
 
 app = create_app('default')
 app_context = app.app_context()
